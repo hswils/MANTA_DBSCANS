bbe59000aa2d46a34c9df682ebb5ff834e914b7e
Haley Wilson <hsw2119@columbia.edu>
2026-02-24 11:13:59 -0800

    half of valid npar values with large files removed including those that ran but had extremely long ray trajectories ie not single pass

---
+++ SCANS_GENRAY/npar_thgrill_scan/clean.sh
@@ -0,0 +1,8 @@
+rm -rf ips*
+rm -rf run.log*
+rm -rf dakota*
+rm -rf params.in.*
+rm -rf SUMMARY
+rm -rf SCAN
+rm -rf results.out*
+rm -rf summary
---
+++ SCANS_GENRAY/npar_thgrill_scan/rmlargefiles.sh
@@ -0,0 +1,16 @@
+rm -rf run.log*
+rm -rf dakota*
+rm -rf params.in.*
+cp *.ipynb /pscratch/sd/h/hswils/github_repos/MANTA_DBSCANS/ANALYSIS_SCRIPTS/
+rm *.ipynb
+rm -rf .ipynb_checkpoints
+rm -rf summary
+rm -rf SCAN/simulation_*.conf
+rm -rf SCAN/work/DAKOTA_BRIDGE*
+rm -rf SCAN/work/FWK_COMP*
+rm -rf SCAN/simulation_*/simulation_*
+rm -rf SCAN/simulation_*/work/fastran_driver*
+rm -rf SCAN/simulation_*/work/fastran_lh_genray_*/xgenray.log
+rm -rf SCAN/simulation_*/work/fastran_lh_genray_*/plot.ps
+rm -rf SCAN/simulation_*/work/plasma_state
+
